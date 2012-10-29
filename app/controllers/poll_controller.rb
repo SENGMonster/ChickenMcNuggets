585a8adadc23b49a6872125cba2a41371eaa0f60
moeje <moe.je@web.de>
2012-10-29 15:07:46 +0100

    session variable

--- app/controllers/poll_controller.rb
+++ app/controllers/poll_controller.rb
@@ -1,4 +1,6 @@
 class PollController < ApplicationController
+  before_filter :check_session, :only => [:vote]
+  
   def show
     @poll=Poll.find(params[:id])
     @answers = Array.new
@@ -25,15 +27,23 @@ class PollController < ApplicationController
     @answer_possibility = AnswerPossibility.find(params[:answer][:id])
     @answer.answer_possibility_id = @answer_possibility.id
     @answer.value = @answer_possibility.value
+    session[@poll.id] = 1
     respond_to do |format|
       if @answer.save
         format.html { redirect_to :action => "show", :id => @poll.id }
         format.json { render json: @poll, status: :created, location: @poll }
       else
         format.html { render action: "vote" }
-        format.json { render json: @club.errors, status: :unprocessable_entity }
+        format.json { render json: @poll.errors, status: :unprocessable_entity }
       end
     end    
+  end
+  
+  def check_session
+    @poll=Poll.find(params[:id])
+    if session[@poll.id]
+      redirect_to :action => "show", :id => @poll.id
+    end
   end
   
 end
--- app/models/poll.rb
+++ app/models/poll.rb
@@ -2,4 +2,5 @@ class Poll < ActiveRecord::Base
   attr_accessible :title, :questin, :exp_date, :user_id, :poll_type_id
   has_many :answer_possibilities
   belongs_to :poll_type
+  
 end
