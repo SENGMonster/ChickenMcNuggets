cf10f56cac34ef8abdbbead0667db2d0c4bd8af5
=MonaFHM <studium@ramonaleenings.com>
2012-11-13 09:00:16 +0100

    Form umgebaut für Polls

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,8 +1,9 @@
 class Poll < ActiveRecord::Base
-  attr_accessible :title, :question, :exp_date, :user_id, :poll_type_id, :category_id
+  attr_accessible :title, :question, :exp_date, :user_id, :poll_type_id, :category_id, :answer_possibilities_attributes
   has_many :answer_possibilities
   belongs_to :poll_type
   belongs_to :category
   has_many :answers
   
+  accepts_nested_attributes_for :answer_possibilities
 end
